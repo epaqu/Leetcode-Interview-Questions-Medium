652720df6e6996448224672395db08cf6a5ba720
An <30442559+epaqu@users.noreply.github.com>
2018-09-08 14:00:09 +0900

    Create sum of two integers.py
    Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.

---
+++ others/sum of two integers.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Solution:
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        return a+b
+        """
+        return sum([a,b])
