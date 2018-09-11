7c4e6403259bd9dbb5239e7f3c6cdadaed3c95a4
An <30442559+epaqu@users.noreply.github.com>
2018-09-11 17:41:53 +0900

    Create Top K Frequent Elements.py
    Given a non-empty array of integers, return the k most frequent elements.
    You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
    Your algorithm's time complexity must be better than O(n log n), where n is the array's size.

---
+++ sorting_and_searching/Top K Frequent Elements.py
@@ -0,0 +1,9 @@
+import collections
+class Solution:
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        return [row[0] for row in collections.Counter(nums).most_common(k)]
